f09f0ff574f89c14c4435c3b7d7de966f0315ad1
Sven <svenlees@hotmail.com>
2025-01-29 16:44:21 +1100

    Iterating through sensors json

--- index.js
+++ index.js
@@ -11,4 +11,25 @@ console.log('Server running on port: ', port)
 const { execSync } = require('child_process');
 let options = { encoding: 'utf8', stdio: ['stdout'] }
 let reading = JSON.parse(execSync('sensors -j', options))
-console.log(reading)
+
+// So lots of things will now happen.... I'll firstly need to run this on a loop and store the results somehow.
+// The way to go might be to have that dict of timestamps and readings under each.
+
+// Device: { Adapter: Adapter type, sensor: { sensor: reading } }
+
+
+
+
+let adapter = ''
+let recAtThisTime = []
+for (const [device, lowerObj] of Object.entries(reading)) {
+    for (const [adaptersensor, adaptypeOrSensor] of Object.entries(lowerObj)) {
+        if (adaptersensor === 'Adapter') {
+
+        } else {
+            for (const [sensorNum, value] of Object.entries(adaptypeOrSensor)) {
+                console.log(value)
+            }
+        }
+    }
+}
